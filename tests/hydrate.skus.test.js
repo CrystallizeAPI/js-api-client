3b0d0342b4371d98d6bbe8ce91ef67b629664576
Håkon Krogh <hakonhk@gmail.com>
2023-02-02 15:02:52 +0100

    Adding a simple test

--- tests/hydrate.skus.test.js
+++ tests/hydrate.skus.test.js
@@ -11,3 +11,15 @@ test('Hydrate Skus', async () => {
     expect(response.product0.path).toBe('/shop/bathroom-fitting/large-mounted-cabinet-in-treated-wood');
     expect(response.product1.path).toBe('/shop/bathroom-fitting/mounted-bathroom-counter-with-shelf');
 });
+
+test('Hydrate Skus by using the sync API', async () => {
+    const CrystallizeClient = createClient({
+        tenantIdentifier: 'furniture',
+    });
+
+    const hydrater = createProductHydrater(CrystallizeClient, { useSyncApiForSKUs: true }).bySkus;
+    const response = await hydrater(['b-1628520141076', 'b-1628514494819'], 'en');
+
+    expect(response.product0.path).toBe('/shop/bathroom-fitting/large-mounted-cabinet-in-treated-wood');
+    expect(response.product1.path).toBe('/shop/bathroom-fitting/mounted-bathroom-counter-with-shelf');
+});
